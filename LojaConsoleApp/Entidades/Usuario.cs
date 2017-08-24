3108f13c01390a17480a518825db52f4d4df9e31
willcevassi <willian.cevassi@outlook.com.br>
2017-08-23 22:17:47 -0300

    Criacao do projeto

---
+++ LojaConsoleApp/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaConsoleApp.Entidades
+{
+    public class Usuario
+    {
+        public virtual int Id { get; set; }
+        public virtual string Nome { get; set; }
+    }
+}
